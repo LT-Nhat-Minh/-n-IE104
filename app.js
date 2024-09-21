2fd982dbaf34720825b4f0e054bc9e5b8c2a045e
LT-Nhat-Minh <22520863@gm.uit.edu.vn>
2024-09-21 23:46:38 +0700

    Update hotel slider

---
+++ app.js
@@ -0,0 +1,32 @@
+let slider = document.querySelector('.slider')
+let list = document.querySelector('.slider_list');
+let items = document.querySelectorAll('.slider_list_item');
+let prev = document.getElementsByClassName('b_prev')[0];
+let next = document.getElementsByClassName('b_next')[0];
+// let test = document.getElementById('test');
+// let test2 = document.getElementById('test2');
+
+let active = 0;
+let lengthItems = items.length -1;
+
+next.onclick = function(){
+    if(items[active+2].offsetLeft <= list.offsetWidth - slider.offsetWidth){
+        active = active + 2;
+        list.style.left = -items[active].offsetLeft -72 + 'px';
+    }
+    else{
+        list.style.left = - (list.offsetWidth - slider.offsetWidth) + 'px';
+    }
+}
+
+prev.onclick = function(){
+    if(active - 2 >= 0){
+        active -= 2;
+        list.style.left = -items[active].offsetLeft - 72 + 'px';
+    }
+    else{
+        list.style.left=0;
+    }    
+    
+}
+
